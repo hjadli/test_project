4135f0fd9ecc537398e8af9bbadf2d494493ab9f
hjadli <inbinciblehappy@gmail.com>
2024-06-04 15:01:35 +0100

    user and admin dashboard working now.

---
+++ AdminSystem/Admin/AddCategory.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AddCategory.aspx.cs" Inherits="AdminDashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <title>Admin Dashboard</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+    </form>
+</body>
+</html>
--- AdminSystem/Admin/Admin.aspx.cs
+++ AdminSystem/Admin/Admin.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,29 +8,56 @@ using System.Web.UI.WebControls;
 
 public partial class AdminDashboard : System.Web.UI.Page
 {
+    private ClsUsersCollection usersCollection = new ClsUsersCollection();
+    // Create OrderCollection and ReportCollection as per your implementation
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
             // Check if user is logged in
-            if (Session["UserId"] != null && Session["Role"] != null && Session["Role"].ToString() == "admin")
-            {
-                string firstName = Session["FirstName"].ToString();
-                lblAdminWelcome.Text = "Welcome, " + firstName + "!";
-            }
-            else
+            if (Session["UserId"] == null || Session["Role"] == null || Session["Role"].ToString() != "admin")
             {
                 // If no user is logged in or user is not admin, redirect to login page
                 Response.Redirect("~/Login.aspx");
             }
+            else
+            {
+                
+
+                // Load dashboard data
+                LoadDashboardData();
+            }
         }
     }
 
-    protected void btnLogout_Click(object sender, EventArgs e)
+    protected void LoadDashboardData()
+    {
+        // Get count of registered users
+        int registeredUsers = usersCollection.Count;
+        lblRegisteredUsers.Text = registeredUsers.ToString();
+
+        
+    }
+
+   
+
+   
+
+    protected void BtnAddProduct_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/AddProduct.aspx");
+    }
+
+    protected void BtnAddCategory_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/AddCategory.aspx");
+    }
+
+    protected void BtnLogout_Click(object sender, EventArgs e)
     {
         // Clear the session
         Session.Clear();
-
         // Redirect to login page
         Response.Redirect("~/Login.aspx");
     }
